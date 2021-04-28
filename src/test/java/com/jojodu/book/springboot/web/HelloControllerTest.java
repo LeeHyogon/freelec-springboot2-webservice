ac53f2cd99001c492e1164d4f88e269ba154119d
이효곤 <LeeHyogon>
2021-04-29 01:14:15 +0900

    5장 스프링 시큐리티 테스트 코드 문제해결

--- src/test/java/com/jojodu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojodu/book/springboot/web/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.jojodu.book.springboot.web;
+import com.jojodu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,13 +14,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE,classes= SecurityConfig.class)
+        }
 
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception{
         String hello="hello";
@@ -24,6 +33,8 @@ public class HelloControllerTest {
 
     }
 
+
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name="hello";
